30989fd4594fe9d3719def7fe2ec65e6622c95c0
brendan <brendan@localhost>
2019-04-05 22:04:07 +0100

    Fix compilation

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
@@ -27,7 +27,7 @@ fn main() {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!"),
+                println!("You win!");
                 break;
             }
         }
